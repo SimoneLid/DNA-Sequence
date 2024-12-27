cdac1a1b85fb0500a80b88f40745ccc5be97c82d
Marco Casu <mcasu299@gmail.com>
2024-12-27 19:20:49 +0100

    prova

--- src/rng.c
+++ src/rng.c
@@ -1,4 +1,4 @@
-/*
+/* pollo
  * Simple random generator
  * LCG (Linear Congruential Generator)
  *
@@ -9,27 +9,29 @@
  *
  * (c) 2024, Arturo Gonzalez-Escribano
  */
-#include<stdint.h>
-#include<math.h>
+#include <stdint.h>
+#include <math.h>
 
 /*
  * Constants
  */
 #define RNG_MULTIPLIER 6364136223846793005ULL
-#define RNG_INCREMENT  1442695040888963407ULL
+#define RNG_INCREMENT 1442695040888963407ULL
 
 /*
  * Type for random sequences state
  */
-typedef uint64_t	rng_t;
+typedef uint64_t rng_t;
 
 /*
  * Constructor: Create a new state from a seed
  */
 #ifdef __CUDACC__
-__host__ __device__ 
+__host__ __device__
 #endif
-rng_t rng_new(uint64_t seed) {
+    rng_t
+    rng_new(uint64_t seed)
+{
     uint64_t hash = seed;
     hash = (hash ^ (hash >> 30)) * 0xbf58476d1ce4e5b9ULL;
     hash = (hash ^ (hash >> 27)) * 0x94d049bb133111ebULL;
@@ -42,26 +44,30 @@ rng_t rng_new(uint64_t seed) {
  * Adapted from the implementation on PCG (https://www.pcg-random.org/)
  */
 #ifdef __CUDACC__
-__host__ __device__ 
+__host__ __device__
 #endif
-double rng_next(rng_t *seq) {
-    *seq = ( *seq * RNG_MULTIPLIER + RNG_INCREMENT);
-    return (double) ldexpf( *seq, -64 );
+    double
+    rng_next(rng_t *seq)
+{
+    *seq = (*seq * RNG_MULTIPLIER + RNG_INCREMENT);
+    return (double)ldexpf(*seq, -64);
 }
 
 /*
  * Next Normal: Advance state and return a double number distributed with a normal(mu,sigma)
  */
 #ifdef __CUDACC__
-__host__ __device__ 
+__host__ __device__
 #endif
-double rng_next_normal( rng_t *seq, double mu, double sigma) {
+    double
+    rng_next_normal(rng_t *seq, double mu, double sigma)
+{
     double u1 = rng_next(seq);
     double u2 = rng_next(seq);
 
     double z0 = sqrt(-2.0 * log(u1)) * cos(2.0 * M_PI * u2);
     // double z1 = sqrt(-2.0 * log(u1)) * sin(2.0 * M_PI * u2);
-    
+
     return mu + sigma * z0;
 }
 
@@ -70,16 +76,20 @@ double rng_next_normal( rng_t *seq, double mu, double sigma) {
  * Adapted from the implementation on PCG (https://www.pcg-random.org/)
  */
 #ifdef __CUDACC__
-__host__ __device__ 
+__host__ __device__
 #endif
-void rng_skip( rng_t *seq, uint64_t steps ) {
+    void
+    rng_skip(rng_t *seq, uint64_t steps)
+{
     uint64_t cur_mult = RNG_MULTIPLIER;
     uint64_t cur_plus = RNG_INCREMENT;
 
     uint64_t acc_mult = 1u;
     uint64_t acc_plus = 0u;
-    while (steps > 0) {
-        if (steps & 1) {
+    while (steps > 0)
+    {
+        if (steps & 1)
+        {
             acc_mult *= cur_mult;
             acc_plus = acc_plus * cur_mult + cur_plus;
         }
@@ -89,4 +99,3 @@ void rng_skip( rng_t *seq, uint64_t steps ) {
     }
     *seq = acc_mult * (*seq) + acc_plus;
 }
-
