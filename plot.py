c836d1fb2cebaebbb532d1c708a9ee74c7e519cb
SimoneLid <simlid4@gmail.com>
2025-01-19 22:07:25 +0100

    Cambiati grafici OpenMP

--- plot.py
+++ plot.py
@@ -1,8 +1,11 @@
 from matplotlib import pyplot as plt
 
 x_values = [1,2,4,8,16,32]
-y_values = [1,1.928686,3.826311,7.51051,14.716016,24.241052]
+y_values = [71.273433,36.941176,18.620522,9.486428,4.841522,2.939143]
+plt.xticks([1,2,4,8,16,32])
+plt.yticks([71.273433,36.941176,18.620522,9.486428,4.841522,2.939143])
+plt.grid()
 plt.plot(x_values,y_values, marker='o')
 plt.xlabel("numero thread")
-plt.ylabel("speedup")
+plt.ylabel("tempo")
 plt.show()
