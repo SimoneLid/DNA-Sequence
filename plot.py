8541278cac16c9df5f584511d59c5dd4ceb1a0b1
Marco Casu <mcasu299@gmail.com>
2025-01-19 21:42:31 +0100

    relazione

--- plot.py
+++ plot.py
@@ -1,11 +1,11 @@
 from matplotlib import pyplot as plt
 
-x_values = [1,2,4,8,16,32]
-y_values = [72.27,36.94,18.62,9.48,4.84,2.93]
+x_values = [32,64,96,128,160,192,224,256]
+y_values = [2.297742,1.193476,0.826267,0.788565,0.636911,0.526974,0.503687,0.449722]
 plt.plot(x_values,y_values, marker='o')
-plt.xticks([1,2,4,8,16,32])
+plt.xticks([32,64,96,128,160,192,224,256])
 plt.xlabel("numero thread")
-plt.ylabel("tempo")
+plt.ylabel("tempo (caso migliore)")
 plt.grid(True) 
-plt.yticks([72.27,36.94,18.62,9.48,2.93])
+plt.yticks([2.297742,1.193476,0.826267,0.636911,0.526974,0.449722])
 plt.show()
