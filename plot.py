f51f1b0646166c86bd6b3062a34b5a6dc44828e7
Marco Casu <mcasu299@gmail.com>
2025-01-19 19:18:49 +0100

    modificata relazione

--- plot.py
+++ plot.py
@@ -1,8 +1,11 @@
 from matplotlib import pyplot as plt
 
 x_values = [1,2,4,8,16,32]
-y_values = [1,1.928686,3.826311,7.51051,14.716016,24.241052]
+y_values = [72.27,36.94,18.62,9.48,4.84,2.93]
 plt.plot(x_values,y_values, marker='o')
+plt.xticks([1,2,4,8,16,32])
 plt.xlabel("numero thread")
-plt.ylabel("speedup")
+plt.ylabel("tempo")
+plt.grid(True) 
+plt.yticks([72.27,36.94,18.62,9.48,2.93])
 plt.show()
