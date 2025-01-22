7050d92659120752abafe11a267cfdfcae2493bf
Marco Casu <mcasu299@gmail.com>
2025-01-22 18:12:10 +0100

    CUDA

--- plot.py
+++ plot.py
@@ -1,9 +1,11 @@
 from matplotlib import pyplot as plt
 
-x_values = [1,2,4,8,16,32]
-y_values = [1,1.928686,3.826311,7.51051,14.716016,24.241052]
+x_values = [1,256,512,1024,2048,4608]
+y_values = [1,1.76,4.98,5.98,10.83,8.73 ]
 plt.plot(x_values,y_values, marker='o')
-plt.xticks([32,64,96,128,160,192,224,256])
+plt.xticks(x_values)
+plt.yticks(y_values)
+plt.grid(True)
 plt.xlabel("numero thread")
-plt.ylabel("speedup")
+plt.ylabel("speed up")
 plt.show()
